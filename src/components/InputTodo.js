99ab8372d5e8105343832cf77231626fc5f45826
SamanAtashi <milady00126@gmail.com>
2021-08-23 15:53:07 +0300

    Add input + form functionality

---
+++ src/components/InputTodo.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class InputTodo extends Component {
+	state = {
+		title: '',
+	};
+
+	handleInput = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.addTodoProps(this.state.title);
+		this.setState({
+			title: '',
+		});
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input
+					type="text"
+					placeholder="Add Todo..."
+					value={this.state.title}
+					name="title"
+					onChange={this.handleInput}
+				/>
+				<button>Submit</button>
+			</form>
+		);
+	}
+}
+export default InputTodo;
