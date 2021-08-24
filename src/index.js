5f31979493c6151372a17d00739692fe8d192b14
SamanAtashi <milady00126@gmail.com>
2021-08-24 15:48:13 +0300

    Add gh-pages-2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom';
 import TodoContainer from './functionBased/components/TodoContainer';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
+		<Router basename={process.env.PUBLIC_URL}>
 			<TodoContainer />
 		</Router>
 	</React.StrictMode>,
